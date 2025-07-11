e9bda6c1236e26d4d7774fe1192fbc12ca617f30
eshantdasBuzzboard <eshant@buzzboard.com>
2025-07-11 20:04:43 +0530

    last

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ import sys
 import os
 
 # Mount templates with absolute path
-import os
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 import logging
 
 from .routers import home, api
@@ -23,9 +22,6 @@ template_path = os.path.join(
 )
 templates = Jinja2Templates(directory=template_path)
 
-# Mount static files
-static_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "app", "static")
-app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 # Include routers
 app.include_router(home.router)
